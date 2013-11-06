ace9f4f70e3929f56688a469e1148c9485033156
phitsc <phitsc@gmail.com>
2013-11-06 23:43:28 +0100

    Adding source

---
+++ MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TextManipulationUtility
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.manipulationsTreeView = new System.Windows.Forms.TreeView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.manipulationsTreeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(996, 585);
+            this.splitContainer1.SplitterDistance = 331;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // manipulationsTreeView
+            // 
+            this.manipulationsTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.manipulationsTreeView.HideSelection = false;
+            this.manipulationsTreeView.Location = new System.Drawing.Point(0, 0);
+            this.manipulationsTreeView.Name = "manipulationsTreeView";
+            this.manipulationsTreeView.Size = new System.Drawing.Size(331, 585);
+            this.manipulationsTreeView.TabIndex = 0;
+            this.manipulationsTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.manipulationsTreeView_AfterSelect);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.inputTextBox);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.outputTextBox);
+            this.splitContainer2.Size = new System.Drawing.Size(661, 585);
+            this.splitContainer2.SplitterDistance = 257;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(661, 257);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "";
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(661, 324);
+            this.outputTextBox.TabIndex = 0;
+            this.outputTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 585);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "Text Inspection & Manipulation Utility";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView manipulationsTreeView;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,433 @@
+﻿namespace TextManipulationUtility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        private TextInfo textInfo = System.Globalization.CultureInfo.CurrentCulture.TextInfo;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            PopulateManipulations();
+
+            inputTextBox.Text = Clipboard.GetText();
+        }
+
+        void PopulateManipulations()
+        {
+            var count = manipulationsTreeView.Nodes.Add("Count");
+            count.Nodes.Add("Chars, Words, Lines");
+            count.Nodes.Add("Alphabet");
+            count.Nodes.Add("Alphabet case-insensitive");
+
+            var caps = manipulationsTreeView.Nodes.Add("Capitalisation");
+            caps.Nodes.Add("Upper");
+            caps.Nodes.Add("Lower");
+            caps.Nodes.Add("Camel");
+
+            var order = manipulationsTreeView.Nodes.Add("Order");
+            order.Nodes.Add("Reverse");
+            order.Nodes.Add("Scramble");
+
+            var checksums = manipulationsTreeView.Nodes.Add("Checksums");
+            checksums.Nodes.Add("MD5");
+            checksums.Nodes.Add("SHA-256");
+
+            var sort = manipulationsTreeView.Nodes.Add("Sort");
+            sort.Nodes.Add("A-Z");
+            sort.Nodes.Add("Z-A");
+
+            var web = manipulationsTreeView.Nodes.Add("Web");
+            web.Nodes.Add("Source");
+            //web.Nodes.Add("Render");
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            updateOutput();
+        }
+
+        private void manipulationsTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            updateOutput();
+        }
+
+        private void updateOutput()
+        {
+            switch (manipulationsTreeView.SelectedNode.Text)
+            {
+                case "Count":
+                case "Chars, Words, Lines":
+                    outputTextBox.Text = count(inputTextBox.Text);
+                    break;
+
+                case "Alphabet":
+                    outputTextBox.Text = countAlphabet(inputTextBox.Text, false);
+                    break;
+
+                case "Alphabet case-insensitive":
+                    outputTextBox.Text = countAlphabet(inputTextBox.Text, true);
+                    break;
+
+                case "Capitalisation":
+                case "Upper":
+                    outputTextBox.Text = upper(inputTextBox.Text);
+                    break;
+
+                case "Lower":
+                    outputTextBox.Text = lower(inputTextBox.Text);
+                    break;
+
+                case "Camel":
+                    outputTextBox.Text = camel(inputTextBox.Text);
+                    break;
+
+                case "Order":
+                case "Reverse":
+                    outputTextBox.Text = reverse(inputTextBox.Text);
+                    break;
+
+                case "Scramble":
+                    outputTextBox.Text = scramble(inputTextBox.Text);
+                    break;
+
+                case "Checksums":
+                    outputTextBox.Text = checksums(inputTextBox.Text);
+                    break;
+
+                case "MD5":
+                    outputTextBox.Text = md5(inputTextBox.Text);
+                    break;
+
+                case "SHA-256":
+                    outputTextBox.Text = sha256(inputTextBox.Text);
+                    break;
+
+                case "Sort":
+                case "A-Z":
+                    outputTextBox.Text = sortAZ(inputTextBox.Text);
+                    break;
+
+                case "Z-A":
+                    outputTextBox.Text = sortZA(inputTextBox.Text);
+                    break;
+
+                case "Web":
+                case "Source":
+                    outputTextBox.Text = webSource(inputTextBox.Text);
+                    break;
+
+                case "Render":
+                    outputTextBox.Text = webRender(inputTextBox.Text);
+                    break;
+            }
+        }
+
+        private string count(string input)
+        {
+            int characterCount = input.Count();
+            int wordCount = input.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            int lineCount = input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            return String.Format("{0} characters. {1} words. {2} lines.", characterCount, wordCount, lineCount);
+        }
+
+        private string countAlphabet(string input, bool caseInsensitive)
+        {
+            var dict = new SortedDictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (char.IsLetter(character))
+                {
+                    var letter = caseInsensitive ? char.ToLower(character) : character;
+
+                    if (dict.ContainsKey(letter))
+                    {
+                        dict[letter]++;
+                    }
+                    else
+                    {
+                        dict.Add(letter, 1);
+                    }
+                }
+            }
+
+            return string.Join(" ", dict);
+        }
+
+        private string upper(string input)
+        {
+            return textInfo.ToUpper(input);
+        }
+
+        private string lower(string input)
+        {
+            return textInfo.ToLower(input);
+        }
+
+        private string camel(string input)
+        {
+            return textInfo.ToTitleCase(input);
+        }
+
+        private string reverse(string input)
+        {
+            var c = input.ToCharArray();
+            Array.Reverse(c);
+            return new String(c);
+        }
+
+        private string scramble(string input)
+        {
+            var c = input.ToCharArray();
+            var rnd = new Random();
+            return new String(c.OrderBy(x => rnd.Next()).ToArray());
+        }
+
+        private string formatHash(byte[] hash)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private string md5(string input)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                return formatHash(md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input)));
+            }
+        }
+
+        private string sha256(string input)
+        {
+            using (var hasher = new System.Security.Cryptography.SHA256Managed())
+            {
+                return formatHash(hasher.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input)));
+            }
+        }
+
+        private string checksums(string input)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("MD5\t" + md5(input) + "\n");
+            sb.Append("SHA-256\t" + sha256(input) + "\n");
+
+            return sb.ToString();
+        }
+
+        private string sortAZ(string input)
+        {
+            var words = input.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(words);
+            return string.Join(" ", words);
+        }
+
+        private string sortZA(string input)
+        {
+            var words = input.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(words);
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+
+        private string webSource(string input)
+        {
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    return webClient.DownloadString(input);
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+            }
+        }
+
+        private string webRender(string input)
+        {
+            string result;
+
+            var source = webSource(input);
+
+            // Remove HTML Development formatting
+            // Replace line breaks with space
+            // because browsers inserts space
+            result = source.Replace("\r", " ");
+            // Replace line breaks with space
+            // because browsers inserts space
+            result = result.Replace("\n", " ");
+            // Remove step-formatting
+            result = result.Replace("\t", string.Empty);
+            // Remove repeating speces becuase browsers ignore them
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                                                                  @"( )+", " ");
+
+            // Remove the header (prepare first by clearing attributes)
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*head([^>])*>", "<head>",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"(<( )*(/)( )*head( )*>)", "</head>",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(<head>).*(</head>)", string.Empty,
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // remove all scripts (prepare first by clearing attributes)
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*script([^>])*>", "<script>",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"(<( )*(/)( )*script( )*>)", "</script>",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            //result = System.Text.RegularExpressions.Regex.Replace(result, 
+            //         @"(<script>)([^(<script>\.</script>)])*(</script>)",
+            //         string.Empty, 
+            //         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"(<script>).*(</script>)", string.Empty,
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // remove all styles (prepare first by clearing attributes)
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*style([^>])*>", "<style>",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"(<( )*(/)( )*style( )*>)", "</style>",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(<style>).*(</style>)", string.Empty,
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // insert tabs in spaces of <td> tags
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*td([^>])*>", "\t",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // insert line breaks in places of <BR> and <LI> tags
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*br( )*>", "\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*li( )*>", "\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // insert line paragraphs (double line breaks) in place
+            // if <P>, <DIV> and <TR> tags
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*div([^>])*>", "\r\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*tr([^>])*>", "\r\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<( )*p([^>])*>", "\r\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // Remove remaining tags like <a>, links, images,
+            // comments etc - anything thats enclosed inside < >
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<[^>]*>", string.Empty,
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            // replace special characters:
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&nbsp;", " ",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&bull;", " * ",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&lsaquo;", "<",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&rsaquo;", ">",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&trade;", "(tm)",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&frasl;", "/",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"<", "<",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @">", ">",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&copy;", "(c)",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&reg;", "(r)",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            // Remove all others. More can be added, see
+            // http://hotwired.lycos.com/webmonkey/reference/special_characters/
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     @"&(.{2,6});", string.Empty,
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+
+            // make line breaking consistent
+            result = result.Replace("\n", "\r");
+
+            // Remove extra line breaks and tabs:
+            // replace over 2 breaks with 2 and over 4 tabs with 4. 
+            // Prepare first to remove any whitespaces inbetween
+            // the escaped characters and remove redundant tabs inbetween linebreaks
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(\r)( )+(\r)", "\r\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(\t)( )+(\t)", "\t\t",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(\t)( )+(\r)", "\t\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(\r)( )+(\t)", "\r\t",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            // Remove redundant tabs
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(\r)(\t)+(\r)", "\r\r",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            // Remove multible tabs followind a linebreak with just one tab
+            result = System.Text.RegularExpressions.Regex.Replace(result,
+                     "(\r)(\t)+", "\r\t",
+                     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            // Initial replacement target string for linebreaks
+            string breaks = "\r\r\r";
+            // Initial replacement target string for tabs
+            string tabs = "\t\t\t\t\t";
+            for (int index = 0; index < result.Length; index++)
+            {
+                result = result.Replace(breaks, "\r\r");
+                result = result.Replace(tabs, "\t\t\t\t");
+                breaks = breaks + "\r";
+                tabs = tabs + "\t";
+            }
+
+            // Thats it.
+            return result;
+        }
+    }
+}
